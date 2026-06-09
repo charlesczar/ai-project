3ea90408d1837f8a2e38a3f74ee2af333490a449
czar <charlesczarlucas@gmail.com>
2026-06-09 12:39:37 +0800

    initial code

--- RoBERTa/model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-
-def create_model(config):    
-    model = AutoModelForSequenceClassification.from_pretrained(        
-        config.model_name,        
-        num_labels=config.num_labels    
-    )    
-    return model
